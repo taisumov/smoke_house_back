e2f80c8535165f9fba5781d2925947465631fa17
Islam Taisumov <islam.taisumov10@gmail.com>
2022-08-26 01:26:07 +0300

    Initializing project

---
+++ index.js
@@ -0,0 +1,24 @@
+// Импорт
+require('dotenv').config()
+const express = require('express');
+const cors = require('cors')
+const path = require('path')
+
+// Задаем порт
+const PORT = process.env.PORT || 5000
+
+// Инициализируем все необходимые модули и само приложение
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => console.log(`Started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start().then(() => console.log('IS WORKING...'))
