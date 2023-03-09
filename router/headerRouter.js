8c64da91e0494250f1d575bdc2eafa889e6fc856
taisumov <islam.taisumov10@gmail.com>
2023-03-10 00:05:27 +0300

    added visibility on header

--- router/headerRouter.js
+++ router/headerRouter.js
@@ -39,7 +39,7 @@ class HeaderController {
             let newHeaderInfo = await modelGetAll(Header)
 
             if (isAllowed || (newHeaderInfo && isVisible)) {
-                return res.status(200).json(newHeaderInfo)
+                return res.status(200).json({...newHeaderInfo, visible: isVisible})
             } else {
                 return res.status(200).json(undefined)
             }
